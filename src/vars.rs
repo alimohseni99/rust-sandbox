871c0a90986214741762a2e644b0ed3cddd442fb
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-01-31 23:32:12 +0100

    use const

--- src/vars.rs
+++ src/vars.rs
@@ -1,7 +1,13 @@
+use crate::print;
+
 pub fn run(){ 
 let name= "Ali";
 let mut age: i32 = 25;
 
 age = 26;
 println!("My name is {} and I am {}", name, age);
+
+
+const ID: i32 = 001;
+println!("ID: {}", ID);
 } 
