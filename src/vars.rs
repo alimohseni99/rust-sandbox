1a25436e12c30048fa54a323cda289fcc7fbad1f
Ali Mohseni <ali.mohseni05@yahoo.se>
2025-01-31 23:33:13 +0100

    assigned multiples variables

--- src/vars.rs
+++ src/vars.rs
@@ -10,4 +10,8 @@ println!("My name is {} and I am {}", name, age);
 
 const ID: i32 = 001;
 println!("ID: {}", ID);
+
+
+let (my_name, my_age) = ("Ali", 25);
+println!("{} is {}", my_name, my_age);
 } 
